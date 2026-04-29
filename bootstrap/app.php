02075b8dda711753a7fb0d314f341760daa25e46
coreconst <parfilokostya0@gmail.com>
2026-04-29 21:35:43 +0300

    Init commit

---
+++ bootstrap/app.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Foundation\Application;
+use Illuminate\Foundation\Configuration\Exceptions;
+use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Http\Request;
+
+return Application::configure(basePath: dirname(__DIR__))
+    ->withRouting(
+        api: __DIR__.'/../routes/api.php',
+        commands: __DIR__.'/../routes/console.php',
+        health: '/up',
+    )
+    ->withMiddleware(function (Middleware $middleware): void {
+        //
+    })
+    ->withExceptions(function (Exceptions $exceptions): void {
+        $exceptions->renderable(function (Throwable $e, Request $request) {
+            return response()->json([
+                'message' => $e->getMessage(),
+                'status' => $e->getCode(),
+                'trace' => $e->getTraceAsString(),
+            ]);
+        });
+    })->create();
